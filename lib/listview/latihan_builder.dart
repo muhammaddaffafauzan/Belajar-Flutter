93be6a5b4c98d41e44c0c49c87afa1868fb8adbb
muhammaddaffafauzan <mobileasia215@gmail.com>
2024-01-24 11:57:52 +0700

    finish

--- lib/listview/latihan_builder.dart
+++ lib/listview/latihan_builder.dart
@@ -143,8 +143,10 @@ class LatihanListBuilder extends StatelessWidget {
                             margin: EdgeInsets.all(10),
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
                                 image: NetworkImage(itemList[index].gallery),
+                                fit: BoxFit.fill,
                               ),
                             ),
                           ),
@@ -176,8 +178,10 @@ class LatihanListBuilder extends StatelessWidget {
                             margin: EdgeInsets.all(10),
                             padding: EdgeInsets.all(10),
                             decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
                                 image: NetworkImage(itemList[index].pemain),
+                                fit: BoxFit.fill,
                               ),
                             ),
                           ),
